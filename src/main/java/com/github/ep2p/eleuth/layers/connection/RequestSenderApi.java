ff00499964c6e6b15f3f66a4baadda0d90264100
sepehr-gh <sepehrgh01@gmail.com>
2021-01-21 22:37:05 +0330

    fix: removed unrelated method

--- src/main/java/com/github/ep2p/eleuth/layers/connection/RequestSenderApi.java
+++ src/main/java/com/github/ep2p/eleuth/layers/connection/RequestSenderApi.java
@@ -2,18 +2,17 @@ package com.github.ep2p.eleuth.layers.connection;
 
 
 import com.github.ep2p.eleuth.model.ConnectionInfo;
-import com.github.ep2p.eleuth.model.NodeDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Map;
 
 public interface RequestSenderApi {
     <Q, B, RB> void sendRequest(BigInteger nodeId, Request<Q, B> request, ResponseListener<RB> responseListener, Class<RB> rbClass);
     <Q, B, RB> void sendRequest(BigInteger nodeId, ConnectionInfo connectionInfo, Request<Q, B> request, ResponseListener<RB> responseListener, Class<RB> rbClass);
-    <Q, B, RB> void sendRequest(NodeDto nodeDto, Request<Q, B> request, ResponseListener<RB> responseListener, Class<RB> rbClass);
 
     interface ResponseListener<RB> {
         void onResponse(Response<RB> response);
@@ -26,7 +25,8 @@ public interface RequestSenderApi {
     class Response<RB>{
         private RB body;
         private int status;
-        private Map<String, String> headers;
+        @Builder.Default
+        private Map<String, String> headers = new HashMap<>();
     }
 
     @Getter
@@ -37,7 +37,8 @@ public interface RequestSenderApi {
         private String address;
         private Q query;
         private B body;
-        private Map<String, String> headers;
+        @Builder.Default
+        private Map<String, String> headers = new HashMap<>();
     }
 
     enum Method {
