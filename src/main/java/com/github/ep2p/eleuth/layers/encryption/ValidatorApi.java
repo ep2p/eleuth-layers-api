5b95ada774dd17b215f4bf6cfbe73987e559d131
sepehr-gh <sepehrgh01@gmail.com>
2021-01-21 14:45:34 +0330

    feat: add SignedDataProviderApi.java

--- src/main/java/com/github/ep2p/eleuth/layers/encryption/ValidatorApi.java
+++ src/main/java/com/github/ep2p/eleuth/layers/encryption/ValidatorApi.java
@@ -5,7 +5,7 @@ import com.github.ep2p.eleuth.model.NodeType;
 import com.github.ep2p.eleuth.model.RingMemberProofDto;
 import com.github.ep2p.eleuth.model.SignedData;
 
-public interface Validator {
+public interface ValidatorApi {
     boolean isValidNode(SignedData<NodeDto> signedNodeDto, NodeType nodeType);
     boolean isValidRing(SignedData<NodeDto> signedNodeDto, SignedData<RingMemberProofDto> ringMemberProofDtoSignedData);
 }
