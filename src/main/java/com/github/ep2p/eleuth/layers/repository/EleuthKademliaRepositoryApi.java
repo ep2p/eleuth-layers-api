aae22858e946818aa6339be83c4a8939ab09d411
sepehr-gh <sepehrgh01@gmail.com>
2021-01-21 17:54:19 +0330

    feat: add EleuthKademliaRepositoryApi

---
+++ src/main/java/com/github/ep2p/eleuth/layers/repository/EleuthKademliaRepositoryApi.java
@@ -0,0 +1,7 @@
+package com.github.ep2p.eleuth.layers.repository;
+
+import com.github.ep2p.eleuth.model.kad.Key;
+import com.github.ep2p.kademlia.node.KademliaRepository;
+
+public interface EleuthKademliaRepositoryApi extends KademliaRepository<Key, String> {
+}
