e40dd10687a6558dc6589469099b6b5e48ff8217
sepehr-gh <sepehrgh01@gmail.com>
2021-01-22 11:35:12 +0330

    fix: parameter dependencies for RouteApi

--- src/main/java/com/github/ep2p/eleuth/layers/logic/RouteApi.java
+++ src/main/java/com/github/ep2p/eleuth/layers/logic/RouteApi.java
@@ -1,13 +1,14 @@
 package com.github.ep2p.eleuth.layers.logic;
 
 import com.github.ep2p.eleuth.model.BaseResponse;
-import com.github.ep2p.eleuth.model.NodeDto;
 import com.github.ep2p.eleuth.model.route.AvailabilityMessage;
 import com.github.ep2p.eleuth.model.route.AvailabilityReply;
 import com.github.ep2p.eleuth.model.route.DataMessage;
 
+import java.math.BigInteger;
+
 public interface RouteApi {
-    BaseResponse onAvailabilityMessage(NodeDto route, AvailabilityMessage availabilityMessage);
-    BaseResponse onAvailabilityReply(NodeDto route, AvailabilityReply availabilityReply);
-    BaseResponse onDataMessage(NodeDto route, DataMessage dataMessage);
+    BaseResponse onAvailabilityMessage(BigInteger route, AvailabilityMessage availabilityMessage);
+    BaseResponse onAvailabilityReply(BigInteger route, AvailabilityReply availabilityReply);
+    BaseResponse onDataMessage(BigInteger route, DataMessage dataMessage);
 }
