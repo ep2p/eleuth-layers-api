03cf6e3fd59f0f7c0d37e486febc2695e08e7470
sepehr-gh <sepehrgh01@gmail.com>
2021-01-22 22:24:36 +0330

    fix: api and model for Authorization

--- src/main/java/com/github/ep2p/eleuth/layers/connection/AuthorizationAPI.java
+++ src/main/java/com/github/ep2p/eleuth/layers/connection/AuthorizationAPI.java
@@ -1,8 +1,8 @@
 package com.github.ep2p.eleuth.layers.connection;
 
 import com.github.ep2p.eleuth.model.Authorization;
-import com.github.ep2p.eleuth.model.BaseResponse;
+import com.github.ep2p.eleuth.model.AuthorizationResponse;
 
 public interface AuthorizationAPI {
-    BaseResponse authorize(Authorization authorization);
+    AuthorizationResponse authorize(Authorization authorization);
 }
--- src/main/java/com/github/ep2p/eleuth/layers/encryption/ValidatorApi.java
+++ src/main/java/com/github/ep2p/eleuth/layers/encryption/ValidatorApi.java
@@ -6,6 +6,6 @@ import com.github.ep2p.eleuth.model.RingMemberProofDto;
 import com.github.ep2p.eleuth.model.SignedData;
 
 public interface ValidatorApi {
-    boolean isValidNode(SignedData<NodeDto> signedNodeDto, NodeType nodeType);
+    boolean isValidNode(SignedData<NodeDto> signedNodeDto);
     boolean isValidRing(SignedData<NodeDto> signedNodeDto, SignedData<RingMemberProofDto> ringMemberProofDtoSignedData);
 }
