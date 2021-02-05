e6dade1c4c6f47256528b433d9df6a4a3d136ec2
sepehr-gh <sepehrgh01@gmail.com>
2021-02-05 10:34:01 +0330

    feat: made knowledge response optional

--- src/main/java/com/github/ep2p/eleuth/layers/repository/RouteKnowledgeRepositoryApi.java
+++ src/main/java/com/github/ep2p/eleuth/layers/repository/RouteKnowledgeRepositoryApi.java
@@ -3,9 +3,10 @@ package com.github.ep2p.eleuth.layers.repository;
 import com.github.ep2p.eleuth.model.entity.RouteKnowledge;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 public interface RouteKnowledgeRepositoryApi {
     void add(RouteKnowledge routeKnowledge);
-    RouteKnowledge getKnowledge(BigInteger nodeId);
+    Optional<RouteKnowledge> getKnowledge(BigInteger nodeId);
     void unlink(BigInteger nodeId, BigInteger routeId);
 }
