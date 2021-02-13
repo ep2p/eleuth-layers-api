5a2b8fb17fbf604b120ce834c0e07ba903a8c44b
sepehr-gh <sepehrgh01@gmail.com>
2021-02-13 12:05:36 +0330

    feat: implement RoutingTableManager

--- src/main/java/com/github/ep2p/eleuth/layers/repository/RoutingTableManager.java
+++ src/main/java/com/github/ep2p/eleuth/layers/repository/RoutingTableManager.java
@@ -3,7 +3,7 @@ package com.github.ep2p.eleuth.layers.repository;
 import com.github.ep2p.eleuth.model.ConnectionInfo;
 import com.github.ep2p.kademlia.table.BigIntegerRoutingTable;
 
-public interface RoutingTableRepository {
+public interface RoutingTableManager {
     void store(BigIntegerRoutingTable<ConnectionInfo> routingTable);
     BigIntegerRoutingTable<ConnectionInfo> load();
 }
